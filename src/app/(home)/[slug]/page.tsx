4454e494b1b9d83be88b29847d51111fca93a907
IvanPortfolio <ivan.kaminskyi@gmail.com>
2024-08-04 04:47:49 -0500

    -- add broken link

--- src/app/(home)/[slug]/page.tsx
+++ src/app/(home)/[slug]/page.tsx
@@ -6,11 +6,21 @@ export const runtime = 'nodejs';
 
 export async function generateStaticParams() {
     const posts = await getAllPosts(false, { skip: 0, limit: 100 });
-    return posts.map((post) => ({
-        slug: post.slug,
-    }));
+    return [
+        ...posts.map((post) => ({
+            slug: post.slug,
+        })),
+        {
+            slug: 'building-a-dynamic-countdown-timer-with-javascript',
+        },
+    ];
 }
 
 export default function ObsoleteRoute({ params: { slug } }) {
+    if (slug === 'building-a-dynamic-countdown-timer-with-javascript') {
+        permanentRedirect(
+            `/blog/building-a-dynamic-countdown-timer-with-javascript-a-step-by-step-guide`,
+        );
+    }
     permanentRedirect(`/blog/${slug}`);
 }
